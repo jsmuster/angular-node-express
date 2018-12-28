10055acf690d474d4cba020a88387c1c4e473ff8
jsmuster <jsmastery@gmail.com>
2018-12-27 21:32:39 -0500

    latest 12/27

--- src/app/input-user-data-form/input-user-data-form.component.ts
+++ src/app/input-user-data-form/input-user-data-form.component.ts
@@ -14,39 +14,40 @@ export class InputUserDataFormComponent implements OnInit {
 	submitted = false;
 	userForm: FormGroup;
 	guid: string;
+	serviceErrors:any = {};
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router)
   {
   	this.http.get('/api/v1/generate_uid').subscribe((data:any) => {
       this.guid = data.guid;
+    }, error => {
+        console.log("There was an error generating the proper GUID on the server", error);
     });
-
-    //data.guid
   }
 
   invalidFirstName()
   {
-  	return (this.submitted && this.userForm.controls.first_name.errors != null);
+  	return (this.submitted && (this.serviceErrors.first_name != null || this.userForm.controls.first_name.errors != null));
   }
 
   invalidLastName()
   {
-  	return (this.submitted && this.userForm.controls.last_name.errors != null);
+  	return (this.submitted && (this.serviceErrors.last_name != null || this.userForm.controls.last_name.errors != null));
   }
 
   invalidEmail()
   {
-  	return (this.submitted && this.userForm.controls.email.errors != null);
+  	return (this.submitted && (this.serviceErrors.email != null || this.userForm.controls.email.errors != null));
   }
 
   invalidZipcode()
   {
-  	return (this.submitted && this.userForm.controls.zipcode.errors != null);
+  	return (this.submitted && (this.serviceErrors.zipcode != null || this.userForm.controls.zipcode.errors != null));
   }
 
   invalidPassword()
   {
-  	return (this.submitted && this.userForm.controls.password.errors != null);
+  	return (this.submitted && (this.serviceErrors.password != null || this.userForm.controls.password.errors != null));
   }
 
   ngOnInit()
@@ -77,7 +78,10 @@ export class InputUserDataFormComponent implements OnInit {
 	      let path = '/user/' + data.customer.uid;
 
 	      this.router.navigate([path]);
-	    });
+	    }, error =>
+	    {
+	    	this.serviceErrors = error.error.error;
+        });
 
   		this.registered = true;
 
